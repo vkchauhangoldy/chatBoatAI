daacd0d64b46d6d6dff5e733974c1d0c4face627
VINAY KUMAR CHAUHAN <vkchauhan750@gmail.com>
2025-09-28 16:37:59 +0530

    initial setup

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import History from "./pages/History/History";
+import reportWebVitals from "./reportWebVitals";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "history",
+        element: <History />,
+      },
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
