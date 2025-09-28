1a022b935f3b31b9ab52b9cde9a6b2bac8b97f8f
VINAY KUMAR CHAUHAN <vkchauhan750@gmail.com>
2025-09-28 17:04:23 +0530

    production ready

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import History from "./pages/History/History";
+import reportWebVitals from "./reportWebVitals";
 
 const router = createBrowserRouter([
   {
@@ -29,3 +30,5 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+reportWebVitals();
